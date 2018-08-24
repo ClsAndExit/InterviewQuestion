5a7df7dbb113742451ea4ea8652d9c6f2d4c738a
Mr.Zhang <778379861@qq.com>
2018-08-24 20:46:00 +0800

    dataStructure

--- src/dataStructureInNowCoder/BinaryCodedString.java
+++ src/dataStructureInNowCoder/BinaryCodedString.java
@@ -2,6 +2,17 @@ package dataStructureInNowCoder;
 
 public class BinaryCodedString {
     public static void main(String[] args) {
+        /*
+        用二进制来编码字符串“abcdabaa”，需要能够根据编码，解码回原来的字符串，最少需要()长的二进制字符串？
+         采用哈夫曼编码思想
+             a=1
+            b=01
+            c=000
+            d=001
+            位数不足的后面补0
+            3*4 = 12 位
+                 */
+
         String string = "abcdabaa";
 
     }
