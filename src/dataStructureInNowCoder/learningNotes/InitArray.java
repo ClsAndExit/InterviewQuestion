447263df654f195afd0c303541dd1e8c16a7e13f
Mr.Zhang <778379861@qq.com>
2018-08-28 21:44:09 +0800

    。。。。

---
+++ src/dataStructureInNowCoder/learningNotes/InitArray.java
@@ -0,0 +1,11 @@
+package dataStructureInNowCoder.learningNotes;
+
+public class InitArray {
+    public static void main(String[] args) {
+        int [] array = new int[] {10};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
